2e22836b2f7809927e60ed168bb140f8931b7a18
iCODE <iliasbolo@gmail.com>
2024-05-04 01:42:39 +0300

    Add files via upload

--- components/page-contact/Header.jsx
+++ components/page-contact/Header.jsx
@@ -29,7 +29,7 @@ function Header() {
           <div className="col-12">
             <div className="text-center">
               <h1 className="text-u ls1 fz-80">
-                Get In <span className="fw-200"> Touch</span>
+                <span className="fw-200"> ΕΠΙΚΟΙΝΩΝΙΑ</span>
               </h1>
             </div>
           </div>
