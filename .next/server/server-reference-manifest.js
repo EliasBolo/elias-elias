131070b6cc5884c2c7d289e066117d29bed94d6b
iCODE <iliasbolo@gmail.com>
2024-05-04 12:12:26 +0300

    logo

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"T1GDY4h9v9BUta0RGrOyhes7TUgbDBQOLyAbsxtpzP0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"PKx9L+cAnCugIrFUchRln5eSw9pftHEpvsna9PA6OAc=\"\n}"
