db4742d3c6764f0310ae4ed79b85ce4014999070
Elias Bolo <iliasbolo@gmail.com>
2024-05-12 01:06:20 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css';
 import Script from 'next/script';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 
 export default function RootLayout({ children }) {
@@ -8,6 +10,10 @@ export default function RootLayout({ children }) {
   <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/683c0c58275241080fccc16d/script.js" 
   strategy="beforeInteractive"></script>
     </head>
+    <body>
+        {children}
+        <SpeedInsights />
+      </body>
   
   </html>;
 }
