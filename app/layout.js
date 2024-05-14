3c5df52b69a60bb32cfd78db066e59e10147311c
iCODE <iliasbolo@gmail.com>
2024-05-14 22:09:21 +0300

    language

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 export default function RootLayout({ children }) {
-  return <html lang="en">{children}
+  return <html lang="el">{children}
     <head>
   <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/683c0c58275241080fccc16d/script.js" 
   strategy="beforeInteractive"></script>
